62babb9b15796e789fdf2f01118f3dfa59bc54a3
Kishore K <114010446+kishorekmani@users.noreply.github.com>
2023-09-22 13:31:08 +0000

    Updated About Section with details

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,24 +16,22 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/dave-netto-wWYwYiCoVwI-unsplash.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a Third year Computer Science student studying in Panimalar Engineering College, Chennai. I am a Web Developer and a Graphic Designer, who aspires to learn new technologies everyday.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
+  "Web development",
   "Focus group testing",
   "Mobile user interfaces",
   "Graphic design",
@@ -45,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation.";
 
 const About = () => {
   return (
