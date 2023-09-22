71f8fba4e81114b5ccd8e767962ae80bdba26fb6
Kishore K <114010446+kishorekmani@users.noreply.github.com>
2023-09-22 13:51:36 +0000

    Updated App.jsx siteProps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Kishore K",
+  title: "Web Developer & Graphic Designer",
+  email: "27kishorekumar@gmail.com",
+  gitHub: "kishorekmani",
+  instagram: "kishorekmani",
+  linkedIn: "kishorekmani",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
